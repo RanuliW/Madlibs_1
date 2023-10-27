d04629c9bec8c209fe395d628e588b66e3d7b90b
RanuliW <ranuliwickramarathne@gmail.com>
2023-10-27 11:49:30 +0530

    Initial commit

---
+++ madlibs.py
@@ -0,0 +1,19 @@
+adj1 = input('Adjective: ')
+adj2 = input('Adjective: ')
+adj3 = input('Adjective: ')
+time_of_day = input('Time of day: ')
+body_part = input('Body part (plural): ')
+noun = input('Noun: ')
+noun_plural1 = input('Noun (plural): ')
+color = input('Color: ')
+verb_past = input('Verb (past tense): ')
+food = input('Food: ')
+
+madlib = f'It was a {adj1} summer {time_of_day} when we realized that the vaccine to stop \
+spread of the disease did not work. Instead, it produced ZOMBIES!!! They were thirsty for {body_part} \
+and the streets were lined with these monsters holding {noun_plural1} in their hands. \
+Their eyes were {color} with hunger as they {verb_past} around the city looking for {food}. \
+They were {adj2} and {adj3}, unknowing how to act in this human world..... except eat {body_part}!! \
+That was their sole mission and they were dedicated towards achieving it for the sake of {noun}.'
+
+print(madlib)
